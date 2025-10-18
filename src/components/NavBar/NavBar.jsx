42d387ceb656e1634b20a67f5459188de545c21a
itanmay728 <ktanmay1130@gmail.com>
2025-10-18 17:53:47 +0530

    bug fix

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Navbar.module.css";
+import styles from "./NavBar.module.css";
 import { NavLink } from "react-router-dom";
 import logo from '../../assets/logo.png';
 
